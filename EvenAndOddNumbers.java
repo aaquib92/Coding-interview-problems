11a3a6ff5e27b368881787d3ee4f968b841777a8
aaquib92 <nadkar.aaquib@gmail.com>
2025-08-21 11:22:45 +0530

    Program to print even and odd numbers in a given list using stream api.

---
+++ EvenAndOddNumbers.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+public class EvenAndOddNumbers
+{
+	public static void main(String[] args) {
+       List<Integer> numbers = Arrays.asList(11, 23, 98, 34, 15, 32, 42, 80, 99, 100);
+       Map<String, List<Integer>> grouped = numbers.stream().collect(Collectors.groupingBy(n -> n%2 == 0 ? "Even":"Odd"));
+       System.out.println(grouped);
+
+	}
+}
