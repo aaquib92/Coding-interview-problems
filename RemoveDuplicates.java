d7225d002bc44c40feee3951314d12d51762c886
aaquib92 <nadkar.aaquib@gmail.com>
2025-08-21 11:29:38 +0530

    Program to remove duplicates from a given list using stream api.

---
+++ RemoveDuplicates.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+public class RemoveDuplicates
+{
+	public static void main(String[] args) {
+		List<Integer> list = Arrays.asList(1, 2, 3, 4, 2, 3, 5);
+		List<Integer> removeDuplicates = list.stream().distinct().collect(Collectors.toList());
+		System.out.println(removeDuplicates);
+
+
+	}
+}
