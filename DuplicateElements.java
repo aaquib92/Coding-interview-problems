6b00c8ea26d61d9ad21065739e2142e2b1271c88
aaquib92 <nadkar.aaquib@gmail.com>
2025-08-21 11:03:09 +0530

    Program to find duplicate elements in a list using stream api.

---
+++ DuplicateElements.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+public class DuplicateElements
+{
+	public static void main(String[] args) {
+		List<Integer> list = Arrays.asList(1, 2, 3, 4, 2, 3, 5);
+        // Method 1
+		Set<Integer> unique = new HashSet<>();
+		List<Integer> duplicates = list.stream().filter(a -> !unique.add(a)).collect(Collectors.toList());
+		// Method 2
+		Map<Integer,Long> freqCount = list.stream().collect(Collectors.groupingBy(Function.identity(),Collectors.counting()));
+		Set<Integer> duplicates = freqCount.entrySet().stream().filter(entry -> entry.getValue() > 1).map(Map.Entry::getKey).collect(Collectors.toSet());
+		System.out.println(duplicates);
+		
+
+	}
+}
